5e63616f7e46e64b10bf33894f3eb8b6681f5d8b
miranraznatovic <miran.raznatovic@edu.fit.ba>
2022-01-16 12:34:48 +0100

    Zavrseno kreiranje 'kostura' sa nav-barom pripremljenim za implementaciju registracije i logina

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'The Kurs App';
+  title = 'The Dating App';
   users: any;
   
   constructor(private http: HttpClient) {} 
