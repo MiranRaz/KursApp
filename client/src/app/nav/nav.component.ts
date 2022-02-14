428338c89fd64416fa5744185ad65634ed55ebd9
miranraznatovic <miran.raznatovic@edu.fit.ba>
2022-02-14 13:43:51 +0100

    Login and registration added and implemente

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,8 @@
+import { trimTrailingNulls } from '@angular/compiler/src/render3/view/util';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { AccountService } from '../_services/account.service';
+import { User } from '../_models/user';
 
 @Component({
   selector: 'app-nav',
@@ -8,23 +11,22 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
   model: any = {}
-  loggedIn!: boolean;
   
-  constructor(private accountService: AccountService) { } 
+  constructor(public accountService: AccountService) { } 
 
-  ngOnInit(): void {
+  ngOnInit(): void { 
   }
+
 
   login(){
     this.accountService.login(this.model).subscribe(response => {
       console.log(response);
-        this.loggedIn = true;
     },error => {
       console.log(error);      
     })
   }
 
   logout(){
-    this.loggedIn = false;    
+    this.accountService.logout();
   }
 }
